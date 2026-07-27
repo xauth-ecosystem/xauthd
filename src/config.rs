de4c5b546b19449a24d7edc07d9abf3971949634
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-27 23:02:37 +0300

    feat: Parse password_hashing section in config.rs

--- src/config.rs
+++ src/config.rs
@@ -4,21 +4,48 @@ use std::path::Path;
 
 const DEFAULT_CONFIG: &str = include_str!("../xauthd.example.toml");
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct DatabaseSettings {
     pub url: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct NetworkSettings {
     pub grpc_address: String,
     pub web_address: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
+pub struct BcryptSettings {
+    pub cost: u32,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Argon2idSettings {
+    pub memory_cost: u32,
+    pub time_cost: u32,
+    pub threads: u32,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct PasswordHashingOptions {
+    #[serde(rename = "BCRYPT")]
+    pub bcrypt: Option<BcryptSettings>,
+    #[serde(rename = "ARGON2ID")]
+    pub argon2id: Option<Argon2idSettings>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct PasswordHashingSettings {
+    pub algorithm: String,
+    pub options: Option<PasswordHashingOptions>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub network: NetworkSettings,
+    pub password_hashing: PasswordHashingSettings,
 }
 
 impl Settings {
