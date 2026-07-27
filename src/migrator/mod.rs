38b4c96736361daaa53cbe48d7e1b68435b1dbef
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-27 20:37:55 +0300

    chore: Update migrator module to reflect renamed files

--- src/migrator/mod.rs
+++ src/migrator/mod.rs
@@ -1,8 +1,8 @@
 use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_user_table;
-mod m20230101_000002_create_token_blacklist;
-mod m20230101_000003_create_oauth_clients;
+mod m20260727_000001_create_user_table;
+mod m20260727_000002_create_token_blacklist;
+mod m20260727_000003_create_oauth_clients;
 
 pub struct Migrator;
 
@@ -10,9 +10,9 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_user_table::Migration),
-            Box::new(m20230101_000002_create_token_blacklist::Migration),
-            Box::new(m20230101_000003_create_oauth_clients::Migration),
+            Box::new(m20260727_000001_create_user_table::Migration),
+            Box::new(m20260727_000002_create_token_blacklist::Migration),
+            Box::new(m20260727_000003_create_oauth_clients::Migration),
         ]
     }
 }
