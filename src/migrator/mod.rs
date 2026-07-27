15d1d75e64dae51894406619b3388d74a191b66f
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-27 19:12:01 +0300

    feat: Register token_blacklist migration

--- src/migrator/mod.rs
+++ src/migrator/mod.rs
@@ -1,6 +1,7 @@
 use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_user_table;
+mod m20230101_000002_create_token_blacklist;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20220101_000001_create_user_table::Migration),
+            Box::new(m20230101_000002_create_token_blacklist::Migration),
         ]
     }
 }
