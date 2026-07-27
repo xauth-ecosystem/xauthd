103886f30d8e7058d32ace456bf0c658541cdeed
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-27 19:17:23 +0300

    feat: Register oauth_clients migration

--- src/migrator/mod.rs
+++ src/migrator/mod.rs
@@ -2,6 +2,7 @@ use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_user_table;
 mod m20230101_000002_create_token_blacklist;
+mod m20230101_000003_create_oauth_clients;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_user_table::Migration),
             Box::new(m20230101_000002_create_token_blacklist::Migration),
+            Box::new(m20230101_000003_create_oauth_clients::Migration),
         ]
     }
 }
