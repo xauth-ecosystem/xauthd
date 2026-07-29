f4d9963322c9169c3d3c513122d1cf7c341b7829
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-29 03:19:55 +0300

    feat: Add totp_secret to user model

--- src/db.rs
+++ src/db.rs
@@ -15,6 +15,7 @@ pub struct Model {
     pub last_failed_attempt: Option<i64>,
     pub is_banned: bool,
     pub must_change_password: bool,
+    pub totp_secret: Option<String>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
