64a33a8012d71e78bf0a834b1b118381850c172a
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-29 18:16:14 +0300

    refactor: Turn user repository into facade over focused repos

--- src/db/repositories/user.rs
+++ src/db/repositories/user.rs
@@ -1,15 +1,56 @@
 use sea_orm::entity::prelude::*;
 use sea_orm::{ActiveModelTrait, DatabaseConnection, Set};
 
+use crate::db::entities::oauth_clients as oc_entities;
+use crate::db::entities::oauth_tokens as ot_entities;
+use crate::db::entities::sessions as s_entities;
 use crate::db::entities::users::{ActiveModel, Column, Entity, Model};
+use crate::db::repositories::blacklist::BlacklistRepository;
+use crate::db::repositories::oauth_client::OAuthClientRepository;
+use crate::db::repositories::oauth_token::OAuthTokenRepository;
+use crate::db::repositories::session::SessionRepository;
 
 pub struct UserRepository {
     db: DatabaseConnection,
+    sessions: SessionRepository,
+    oauth_clients: OAuthClientRepository,
+    oauth_tokens: OAuthTokenRepository,
+    blacklist: BlacklistRepository,
 }
 
 impl UserRepository {
     pub fn new(db: DatabaseConnection) -> Self {
-        Self { db }
+        let sessions = SessionRepository::new(db.clone());
+        let oauth_clients = OAuthClientRepository::new(db.clone());
+        let oauth_tokens = OAuthTokenRepository::new(db.clone());
+        let blacklist = BlacklistRepository::new(db.clone());
+        Self {
+            db,
+            sessions,
+            oauth_clients,
+            oauth_tokens,
+            blacklist,
+        }
+    }
+
+    pub fn db(&self) -> &DatabaseConnection {
+        &self.db
+    }
+
+    pub fn sessions(&self) -> &SessionRepository {
+        &self.sessions
+    }
+
+    pub fn oauth_clients(&self) -> &OAuthClientRepository {
+        &self.oauth_clients
+    }
+
+    pub fn oauth_tokens(&self) -> &OAuthTokenRepository {
+        &self.oauth_tokens
+    }
+
+    pub fn blacklist(&self) -> &BlacklistRepository {
+        &self.blacklist
     }
 
     pub async fn get_user_by_name(&self, username: &str) -> Result<Option<Model>, DbErr> {
@@ -17,10 +58,6 @@ impl UserRepository {
             .filter(Column::Username.eq(username))
             .one(&self.db)
             .await
-    }
-
-    pub async fn get_user_by_id(&self, user_id: i64) -> Result<Option<Model>, DbErr> {
-        Entity::find_by_id(user_id).one(&self.db).await
     }
 
     pub async fn is_2fa_enabled(&self, user_id: i64) -> Result<bool, DbErr> {
@@ -119,5 +156,82 @@ impl UserRepository {
         };
         update.update(&self.db).await?;
         Ok(())
+    }
+
+    pub async fn blacklist_token(&self, token_jti: &str, expires_at: i64) -> Result<(), DbErr> {
+        self.blacklist.add(token_jti, expires_at).await
+    }
+
+    pub async fn is_token_blacklisted(&self, token_jti: &str) -> Result<bool, DbErr> {
+        self.blacklist.is_blacklisted(token_jti).await
+    }
+
+    pub async fn validate_oauth_client(
+        &self,
+        client_id: &str,
+        client_secret: &str,
+    ) -> Result<bool, DbErr> {
+        self.oauth_clients.validate(client_id, client_secret).await
+    }
+
+    pub async fn get_oauth_client(
+        &self,
+        client_id: &str,
+    ) -> Result<Option<oc_entities::Model>, DbErr> {
+        self.oauth_clients.get(client_id).await
+    }
+
+    pub async fn create_oauth_client(
+        &self,
+        client_id: &str,
+        client_secret: &str,
+        redirect_uris: &str,
+    ) -> Result<(), DbErr> {
+        self.oauth_clients
+            .create(client_id, client_secret, redirect_uris)
+            .await
+    }
+
+    pub async fn create_session(
+        &self,
+        user_id: i64,
+        token: &str,
+        ip: &str,
+        expires_in_sec: i64,
+    ) -> Result<(), DbErr> {
+        self.sessions.create(user_id, token, ip, expires_in_sec).await
+    }
+
+    pub async fn get_session(&self, token: &str) -> Result<Option<s_entities::Model>, DbErr> {
+        self.sessions.get(token).await
+    }
+
+    pub async fn delete_session(&self, token: &str) -> Result<(), DbErr> {
+        self.sessions.delete(token).await
+    }
+
+    pub async fn create_oauth_token(
+        &self,
+        client_id: &str,
+        user_id: i64,
+        access_token: &str,
+        refresh_token: Option<&str>,
+        expires_in_sec: i64,
+        scopes: &str,
+    ) -> Result<(), DbErr> {
+        self.oauth_tokens
+            .create(client_id, user_id, access_token, refresh_token, expires_in_sec, scopes)
+            .await
+    }
+
+    pub async fn get_oauth_token(
+        &self,
+        token: &str,
+    ) -> Result<Option<ot_entities::Model>, DbErr> {
+        self.oauth_tokens.get(token).await
+    }
+
+    pub async fn delete_oauth_token(&self, token: &str) -> Result<(), DbErr> {
+        self.oauth_tokens.delete(token).await
     }
 }
