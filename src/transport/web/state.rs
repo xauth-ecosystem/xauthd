1edebd18fcd0fda05a692b156b308d834fe2c94b
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-29 21:26:05 +0300

    refactor: Extract AppState to transport/web/state.rs

---
+++ src/transport/web/state.rs
@@ -0,0 +1,16 @@
+use crate::grpc_service::{ClientSender, PendingScopeMap};
+use sea_orm::DatabaseConnection;
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+pub struct AppStateInner {
+    pub db: DatabaseConnection,
+    pub settings: Arc<crate::config::Settings>,
+    pub rsa_key: rsa::RsaPrivateKey,
+    pub templates_dir: String,
+    pub grpc_clients: Arc<RwLock<HashMap<String, ClientSender>>>,
+    pub pending_scope_requests: PendingScopeMap,
+}
+
+pub type AppState = Arc<AppStateInner>;
