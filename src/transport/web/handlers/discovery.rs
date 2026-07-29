4fda3d2ff43a7e248a7157b34ae3e634f85ceaf3
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-29 21:31:33 +0300

    refactor: Extract jwks_get and discovery_get handlers to transport/web/handlers/discovery.rs

---
+++ src/transport/web/handlers/discovery.rs
@@ -0,0 +1,20 @@
+use axum::{extract::State, response::IntoResponse, Json};
+use super::state::AppState;
+
+pub async fn jwks_get(State(state): State<AppState>) -> impl IntoResponse {
+    Json(crate::jwt::get_jwks(&state.rsa_key))
+}
+
+pub async fn discovery_get() -> impl IntoResponse {
+    let base_url = "http://localhost:8080";
+    Json(serde_json::json!({
+        "issuer": base_url,
+        "authorization_endpoint": format!("{}/authorize", base_url),
+        "token_endpoint": format!("{}/token", base_url),
+        "jwks_uri": format!("{}/jwks", base_url),
+        "scopes_supported": ["openid", "profile"],
+        "response_types_supported": ["code"],
+        "grant_types_supported": ["authorization_code", "refresh_token"],
+        "id_token_signing_alg_values_supported": ["RS256"]
+    }))
+}
