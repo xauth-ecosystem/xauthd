e54e6d10f94c50d2f10fa855cbba1d493aba2c22
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-31 19:51:51 +0300

    feat: Define MessageBus trait for gRPC broadcast

---
+++ src/services/bus.rs
@@ -0,0 +1,7 @@
+use crate::xauth_v1::CoreCommand;
+
+#[tonic::async_trait]
+pub trait MessageBus: Send + Sync + 'static {
+    /// Broadcasts a command to all connected Minecraft servers
+    async fn broadcast(&self, cmd: CoreCommand) -> Result<(), String>;
+}
